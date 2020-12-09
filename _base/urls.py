dc6dde41590e75dd3972097b8aa60fb8334a9089
Niwgoat <nimafathinimafathi@gmail.com>
2020-12-10 00:01:02 +0330

    feature: Add book_advertisement app

--- _base/urls.py
+++ _base/urls.py
@@ -8,6 +8,7 @@ from django.contrib.staticfiles.urls import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls'), name='rest_framework'),
+    path('api/v1/book-advertise/', include('book_advertisement.urls_v1')),
     path('api/v1/account/', include('account_management.urls_v1')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
